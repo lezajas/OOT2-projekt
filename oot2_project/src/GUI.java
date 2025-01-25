6cbc845b61594c2eecb59f3c01fc1801f68db7a6
lezajas <slezaja@veleri.hr>
2025-01-25 19:08:05 +0100

    projekt

---
+++ oot2_project/src/GUI.java
@@ -0,0 +1,88 @@
+import java.awt.EventQueue;
+import java.sql.*;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GUI {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 414, 252);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Korisničko ime : ");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(10, 11, 147, 31);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblLozinka = new JLabel("Lozinka : ");
+		lblLozinka.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblLozinka.setBounds(10, 53, 147, 31);
+		frame.getContentPane().add(lblLozinka);
+		
+		textField = new JTextField();
+		textField.setBounds(122, 18, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(122, 60, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Prijavi se");
+		btnNewButton.setBounds(265, 59, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnRegistrirajSe = new JButton("Registriraj se");
+		btnRegistrirajSe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnRegistrirajSe.setBounds(246, 156, 120, 23);
+		frame.getContentPane().add(btnRegistrirajSe);
+		
+		JLabel lblNewLabel_1 = new JLabel("Ako nemate korisnički račun registrirajte se:");
+		lblNewLabel_1.setBounds(10, 160, 212, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+	}
+}
---
+++ oot2_project/src/Registracija.java
@@ -0,0 +1,115 @@
+import java.awt.EventQueue;
+import java.sql.*;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Registracija {
+
+	private JFrame frame;
+	private JTextField email;
+	private JTextField ime;
+	private JPasswordField lozinka;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Registracija window = new Registracija();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Registracija() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 416, 203);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Upišite vaš email:");
+		lblNewLabel.setBounds(31, 30, 104, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblUpiiteVaeIme = new JLabel("Upišite vaše ime:");
+		lblUpiiteVaeIme.setBounds(31, 69, 104, 14);
+		frame.getContentPane().add(lblUpiiteVaeIme);
+		
+		JLabel lblUpiiteSiLozinku = new JLabel("Upišite si lozinku:");
+		lblUpiiteSiLozinku.setBounds(31, 106, 104, 14);
+		frame.getContentPane().add(lblUpiiteSiLozinku);
+		
+		email = new JTextField();
+		email.setBounds(145, 27, 86, 20);
+		frame.getContentPane().add(email);
+		email.setColumns(10);
+		
+		ime = new JTextField();
+		ime.setColumns(10);
+		ime.setBounds(145, 66, 86, 20);
+		frame.getContentPane().add(ime);
+		
+		lozinka = new JPasswordField();
+		lozinka.setBounds(145, 103, 86, 20);
+		frame.getContentPane().add(lozinka);
+		
+		JButton btnNewButton = new JButton("Registriraj se");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String imes, emails, lozinkas;
+				imes=ime.getText();
+				emails=email.getText();
+				lozinkas=lozinka.getText();
+				
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					Connection con=DriverManager.getConnection("jdbc:mysql://student.veleri.hr/tmatejcic", "tmatejcic", "31032003tomi");
+					String upit="INSERT INTO OOT_Korisnik(korisnicko_ime, email, lozinka) VALUES (?, ?, ?)";
+					PreparedStatement ps=con.prepareStatement(upit);
+					ps.setString(1, imes);
+					ps.setString(2, emails);
+					ps.setString(3, lozinkas);
+					
+					int ubacenoRedaka=ps.executeUpdate();
+					if (ubacenoRedaka>0) {
+						JOptionPane.showMessageDialog(null, "Uspješno upisani podaci.");
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "Podaci nisu upisani!");
+					}
+						}
+				catch(Exception e1) {
+					JOptionPane.showMessageDialog(null, e1);
+					
+				}
+				
+				
+				
+			}
+		});
+		btnNewButton.setBounds(266, 102, 110, 23);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
